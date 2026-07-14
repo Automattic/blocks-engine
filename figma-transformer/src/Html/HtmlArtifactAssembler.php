55bb0ca7a5e632a566ab3153f546f0d57e1fb2ef
Chris Huber <chubes@extrachill.com>
2026-07-13 21:57:38 -0400

    Reset Figma semantic browser margins (#601)

--- figma-transformer/src/Html/HtmlArtifactAssembler.php
+++ figma-transformer/src/Html/HtmlArtifactAssembler.php
@@ -28,6 +28,7 @@ final class HtmlArtifactAssembler
             'body{margin:0}',
             '.figma-root{position:relative;width:100%;display:flex;flex-direction:column;align-items:center}',
             'p,h1,h2,h3,h4,h5,h6{margin:0}',
+            'blockquote{margin:0}',
             'ul,ol{margin:0;padding:0;list-style:none}',
             'img{display:block;max-width:100%;height:auto}',
             'a.figma-link{display:contents;color:inherit;text-decoration:inherit}',
