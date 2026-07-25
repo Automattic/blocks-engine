05f39be287423a6bc32d9b9f89c387f5770c05e1
Chris Huber <chris.huber@automattic.com>
2026-07-25 18:08:07 -0400

    Resolve physical border shorthands

--- php-transformer/tests/unit/block-style-support-conversion.php
+++ php-transformer/tests/unit/block-style-support-conversion.php
@@ -70,12 +70,19 @@ $asymmetricBorderShorthand = ( new StyleAttributeMapper() )->map(array(
 ));
 $assert(! isset($asymmetricBorderShorthand['style']['border']['width']) && '4px solid #990000' === ($asymmetricBorderShorthand['leftover']['border-left'] ?? ''), '8c: physical border shorthands prevent uniform native width from overriding a distinct side', json_encode($asymmetricBorderShorthand));
 
+$classAsymmetricBorder = ( new HtmlTransformer() )->transform(
+    '<article class="course"><p>Text</p></article>',
+    array('static_css' => '.course{border:1px solid #ccc;border-left:4px solid #900}')
+)->toArray();
+$classAsymmetricBorderAttrs = $classAsymmetricBorder['blocks'][0]['attrs'] ?? array();
+$assert(! isset($classAsymmetricBorderAttrs['style']['border']['width']), '8d: matched physical border shorthands prevent a conflicting native width', json_encode($classAsymmetricBorderAttrs));
+
 $classBorderImage = ( new HtmlTransformer() )->transform(
     '<img class="photo" src="/photo.jpg" alt="Portrait">',
     array('static_css' => '.photo{border-top-width:12.808px;border-right-width:12.808px;border-bottom-width:12.808px;border-left-width:12.808px;border-style:solid;border-color:#fff}')
 )->toArray();
 $classBorderImageAttrs = $classBorderImage['blocks'][0]['attrs'] ?? array();
-$assert('12.808px' === ($classBorderImageAttrs['style']['border']['width'] ?? ''), '8d: matched class CSS physical border widths reach native image support', json_encode($classBorderImageAttrs));
+$assert('12.808px' === ($classBorderImageAttrs['style']['border']['width'] ?? ''), '8e: matched class CSS physical border widths reach native image support', json_encode($classBorderImageAttrs));
 
 $groupHtml = '<div class="hero-row" style="display:flex;justify-content:center;gap:1rem;min-height:100svh;padding:2rem;background:var(--wp--preset--color--base)"><p>Hello</p><p>World</p></div>';
 $groupResult = ( new HtmlTransformer() )->transform($groupHtml, array())->toArray();
