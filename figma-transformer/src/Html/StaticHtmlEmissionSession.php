fb7137ab2ed03b95c802a69160867798ab3ae135
Chris Huber <chris.huber@automattic.com>
2026-08-23 15:48:17 -0400

    refactor(figma): isolate typography token state

--- figma-transformer/src/Html/StaticHtmlEmissionSession.php
+++ figma-transformer/src/Html/StaticHtmlEmissionSession.php
@@ -12,6 +12,8 @@ final class StaticHtmlEmissionSession
     private readonly StaticHtmlPageState $pageState;
     private readonly StaticHtmlLinkState $linkState;
     private readonly StaticHtmlAssetRegistry $assetRegistry;
+    private readonly StaticHtmlTypographyState $typographyState;
+    private readonly TextStyleDeclarationResolver $textStyleDeclarationResolver;
     private readonly PaintStackResolver $paintStackResolver;
     private readonly ChildLayerCompositionResolver $childLayerCompositionResolver;
     private readonly StaticHtmlVectorEvidence $vectorEvidence;
@@ -20,10 +22,13 @@ final class StaticHtmlEmissionSession
     public function __construct(
         StaticHtmlNodeInspector $nodeInspector,
         StaticHtmlValueFormatter $formatter,
+        TypographyModel $typographyModel,
     ) {
         $this->pageState = new StaticHtmlPageState();
         $this->linkState = new StaticHtmlLinkState();
         $this->assetRegistry = new StaticHtmlAssetRegistry($formatter);
+        $this->typographyState = new StaticHtmlTypographyState();
+        $this->textStyleDeclarationResolver = new TextStyleDeclarationResolver($typographyModel, $formatter, $this->typographyState);
         $this->paintStackResolver = new PaintStackResolver($this->assetRegistry, $formatter);
         $this->childLayerCompositionResolver = new ChildLayerCompositionResolver($this->assetRegistry, $formatter);
         $this->vectorEvidence = new StaticHtmlVectorEvidence($formatter, $this->paintStackResolver);
@@ -48,6 +53,16 @@ final class StaticHtmlEmissionSession
     public function assetRegistry(): StaticHtmlAssetRegistry
     {
         return $this->assetRegistry;
+    }
+
+    public function typographyState(): StaticHtmlTypographyState
+    {
+        return $this->typographyState;
+    }
+
+    public function textStyleDeclarationResolver(): TextStyleDeclarationResolver
+    {
+        return $this->textStyleDeclarationResolver;
     }
 
     public function paintStackResolver(): PaintStackResolver
